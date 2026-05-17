7b27ffbb4a6b80f528be6d074cd8e28afba2694e
samriddha poudel <poudelsamriddha46@gmail.com>
2026-05-17 20:45:39 +0545

    Displayed the random word and hint

--- script.js
+++ script.js
@@ -1,7 +1,34 @@
+const wordDisplay= document.querySelector(".worddisplay")
 const keyboardDiv= document.querySelector(".keyboard");
+
+
+
+let currentWord;
+
+const getRandomWord= () =>{
+    const { word,hint}= wordlist[Math.floor(Math.random()* wordlist.length)];
+    currentWord = word;
+    console.log(word);
+    document.querySelector(".hint-msg b").innerText= hint;
+    wordDisplay.innerHTML= word.split("").map(() => `<li class="letter"></li>`).join("");
+}
+
+const initGame= (button, clickedLetter) => {
+    if(currentWord.includes(clickedLetter)){
+        console.log(clickedLetter, "is exists on the word");
+    } else{
+        console.log(clickedLetter, "is not exists on the word");
+    }
+}
+
 
 for( let i=97; i <= 122 ; i++){
     const button= document.createElement("button");
     button.innerText=String.fromCharCode(i);
     keyboardDiv.appendChild(button);
+    button.addEventListener("click", e => initGame (e.target, String.fromCharCode(i)));
 }
+
+
+
+getRandomWord();
---
+++ word-list.js
@@ -0,0 +1,128 @@
+const wordlist =[
+    {
+        word:"guitar",
+        hint:"A musical instrument with strings."
+    },
+
+    {
+word:"oxygen",
+hint:"A colorless, odorless gas essential for life."
+    },
+    {
+        word:"Mountain",
+        hint:"A large natural elevation of the Earth's surface."
+    },
+    {
+         word:"Painting",
+         hint:"An art form using colors on surface to create images or expression."
+    },
+    {
+        word:"astronomy",
+        hint:"The scientific study of celestial objects and phenomena."
+    },
+    {
+        word:"football",
+        hint:"A popular sport played with a spherical ball."
+    },
+
+    {
+        word:"chocolate",
+        hint:"A sweet treat made from cocoa beans."
+    },
+    {
+        word:"butterfly",
+        hint:"An insect with colorful wings and a slender body"
+    },
+    {
+        word:"history",
+        hint:"The study of past events and human civilization"
+    },
+    {
+        word:"Pizza",
+        hint:"A popular Italian dish wiith round flated basse with toppings "
+
+    },
+    {
+        word:"jazz",
+        hint:" A  game of music charaterized by improvisation and syncopation"
+    },
+    {
+        word:"camera",
+        hint:"A device used to capture and store images or videos "
+    },
+    {
+        word:"diamond",
+        hint:"A precious gemstone known for its brilliance and hardness"
+    },
+    {
+        word:"adventure",
+        hint:"An exciting or daring experience"
+    },
+    {
+        word:"science",
+        hint:"The systematic study of the structure and behavior of the physical and natural world."
+
+    },
+    {
+        word:"bicycle",
+        hint:"A human-powered vehicle with two wheels."
+
+    },
+    {
+        word:"sunset",
+        hint:"The daily disappearance of the sun below the horizon."
+    },
+    {
+        word:"astronomy",
+        hint:"The study of celestial objects , space and the universe."
+    },
+    {
+        word:"coffee",
+        hint:"A popular beverage made from roasted seeds"
+
+    },
+    {
+        word:"dance",
+        hint:"A rhythmic movement of the body"
+    },
+    {
+        word:"telescope",
+        hint:"An instrument used to observe distant objects"
+    },
+    {
+        word:"keychain",
+        hint:"A small key holder or attachment"
+
+    },
+
+    {
+        word:"minirals",
+        hint:"Inorganic substances requried for bodily functions"
+    },{
+    word:"beach",
+    hint:"A sandy place by the sea"
+    },
+
+    {
+        word:"cricket",
+        hint:"A popular bat and ball game"
+    },
+
+    {
+        word:"piano",
+        hint:"A musical instrument with keys"
+    },
+
+    {
+        word:"computer",
+        hint:"An electronic device for processing data"
+    },
+    {
+        word:"watch",
+        hint:"A device worn on the wrist"
+    },
+    {
+        word:"compass",
+        hint:"A navigational tool that points to magnetic north"
+    }
+];
